7c203c2b1faa23b80395e90c61e641f14b633792
Rayn Mikhaeal <rmikhaeal@gmail.com>
2024-12-30 11:35:31 -0500

    setup error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,27 @@
 import express from 'express';
+import 'express-async-errors';
 import 'dotenv/config';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-errors';
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello');
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(process.env.AUTH_PORT, () => {
   console.log('Auth listening');
