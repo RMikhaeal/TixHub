195639751ba4f4bc7f3113b47fb77c6e64fd8da8
Rayn Mikhaeal <rmikhaeal@gmail.com>
2025-01-14 16:02:41 -0500

    create client pages

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import buildClient from '../api/build-client';
+
+const Landing = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>Please sign in</h1>;
+};
+
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default Landing;
